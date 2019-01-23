594b03237b180ebf109bb26c6be060a219fd0a16
ai.shlauzer <ai.shlauzer@tensor.ru>
2019-01-24 01:24:39 +0700

    Добавьте файлы проекта.

---
+++ Controls/CustomPasswordBox.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для CustomPasswordBox.xaml
+    /// </summary>
+    public partial class CustomPasswordBox : UserControl
+    {
+        public CustomPasswordBox()
+        {
+            InitializeComponent();
+            textBox.TextChanged += TextChanged;
+        }
+
+        private void TextChanged(object sender, RoutedEventArgs e)
+        {
+            Password = textBox.Text;
+        }
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        public static readonly DependencyProperty PasswordProperty = DependencyProperty.Register(
+            "Password",
+            typeof(string),
+            typeof(CustomPasswordBox),
+            new PropertyMetadata(default(string)));
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            "Icon",
+            typeof(StreamGeometry),
+            typeof(CustomPasswordBox),
+            new PropertyMetadata(default(StreamGeometry)));
+
+        public StreamGeometry Icon
+        {
+            get { return (StreamGeometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+    }
+}
---
+++ Controls/Slider.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для Slider.xaml
+    /// </summary>
+    public partial class Slider : UserControl
+    {
+        public Slider()
+        {
+            InitializeComponent();
+        }
+
+        #region Value
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            "Value",
+            typeof(double),
+            typeof(Slider),
+            new PropertyMetadata(0.0, new PropertyChangedCallback(ValueChanged)));
+
+        private static void ValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            var slider = sender as Slider;
+            slider.ValueChanged();
+        }
+
+        private void ValueChanged()
+        {
+            var ratio = Maximum <= 0 ? 0.0 : Value / Maximum;
+            if (ratio < 0)
+                ratio = 0;
+            if (ratio > 1)
+                ratio = 1;
+            LeftColumn.Width = new GridLength(ratio, GridUnitType.Star);
+            RightColumn.Width = new GridLength(1 - ratio, GridUnitType.Star);
+        }
+        #endregion
+
+        #region Maximum
+        public double Maximum
+        {
+            get { return (double)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaximumProperty = DependencyProperty.Register(
+            "Maximum",
+            typeof(double),
+            typeof(Slider),
+            new PropertyMetadata(100.0));
+        #endregion
+
+    }
+}
