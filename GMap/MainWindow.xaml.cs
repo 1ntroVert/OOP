84172b84af8f22f8fe78f2dabefd0ed9f210a7bc
ai.shlauzer <ai.shlauzer@tensor.ru>
2019-01-27 01:22:34 +0700

    создан проект GMap

---
+++ GMap/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GMap.NET;
+using GMap.NET.MapProviders;
+
+namespace GMap
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MapLoaded(object sender, RoutedEventArgs e)
+        {
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            // choose your provider here
+            Map.MapProvider = OpenStreetMapProvider.Instance;
+            Map.MinZoom = 2;
+            Map.MaxZoom = 17;
+            // whole world zoom
+            Map.Zoom = 2;
+            // lets the map use the mousewheel to zoom
+            Map.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
+            // lets the user drag the map
+            Map.CanDragMap = true;
+            // lets the user drag the map with the left mouse button
+            Map.DragButton = MouseButton.Left;
+        }
+    }
+}
